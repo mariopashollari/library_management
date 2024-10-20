181c38eb67895f68c5bee4b2c2fdee0508722ce9
zhupan2024 <mariopashollari9@gmail.com>
2024-10-20 13:38:42 +0200

    Revert "testing push"
    This reverts commit b8675260851221d71b26f88ec830147e3ea958aa.

--- library_management/src/main/java/org/example/Main.java
+++ library_management/src/main/java/org/example/Main.java
@@ -7,6 +7,5 @@ public class Main {
     public static void main(String[] args) {
         HibernateUtils hibernateUtils = new HibernateUtils();
         SessionFactory sessionFactory = hibernateUtils.getSessionFactory();
-        System.out.println("Test");
     }
 }
